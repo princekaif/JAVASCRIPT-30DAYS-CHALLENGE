68f64a6218328598e8f80e3749631aa9c99ae921
Mohammed Kaif <89538138+princekaif@users.noreply.github.com>
2024-07-13 21:29:56 +0530

    Add files via upload
    DAY 1 CHALLENGE COMPLETED
    30DDAYS-JAVASCRIPT-CHALLENGE

---
+++ DAY1/day1.js
@@ -0,0 +1,58 @@
+//ACTIVITY 1 VARAIABLE DECLARATION
+
+//TASK 1
+        // Declare a Varaible Using Var,assign a Num and Log the value to the console 
+        var Num = 1;
+        console.log(Num);   // output ----------> 1
+
+//TASK 2
+        // Declare a Varaible Using Let,assign a String and Log the value to the console  
+        let str ="day1";
+        console.log(str);
+
+//ACTIVITY 2  CONSTANT DECLARATION
+
+//TASK 3
+          // Declare a Varaible Using const ,assign a Boolean Value and Log the value to the console       
+          const Val=true;
+          console.log(Val);
+       
+//ACTIVITY 3 DATA TYPES
+
+//TASK 4
+        //Create a Variables Of Different Variables and Log them Using Typeof OPerator
+        const a=10;
+        let b="king";
+        let c=true;
+        let d=10.00;
+        const e=["a ","b"];
+        console.log(typeof a);
+        console.log(typeof b);
+        console.log(typeof c);
+        console.log(typeof d);
+        console.log( Array.isArray(e) );
+
+//ACTIVITY 4 REASSIGNING THE VARIABLES
+
+//TASK  5
+      //Declare the varaiable with let and assign a initial value  to it and reassign the values 
+      let value2 =45;
+      console.log(value2);
+       value2 ="kaif";//we can reassign the a value to the same varaiable
+      console.log(value2);
+    
+//ACTIVITY 5 UNDERSTANDING THE CONSTANTS
+
+//TASK 6
+        //Declare the varaiable with CONST and assign a initial value  to it and reassign the values 
+        const value =85;
+        console.log(value);
+     //S   value = 90;//we can reassign the a value to the same varaiable
+
+        console.log(value);
+
+//END OF DAY1 CHALLENGE 
+//BRAINBOX CODE        
+     
+
+                   
