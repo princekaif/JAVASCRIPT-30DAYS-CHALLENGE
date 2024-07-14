6f15ad0fd60df36269273bbf6ada48269099b600
Mohammed Kaif <89538138+princekaif@users.noreply.github.com>
2024-07-14 14:58:40 +0530

    Add files via upload

---
+++ DAY2/day2.js
@@ -0,0 +1,90 @@
+ //DAY2 OPERATORS
+
+ //ACTIVITY 1 ARITHMETHIC OPERATORS
+
+              //TASK1 Write A Program To Add Two Numbers And Console The Output
+              let a=30;
+              let b=20.0;
+              console.log(a+b);
+              //TASK2 Write A Program To sub Two Numbers And Console The Output
+               console.log(a-b);
+              //TASK3 Write A Program To Multiply Two Numbers And Console The Output
+              console.log(a*b);
+               //TASK4 Write A Program To Divide Two Numbers And Console The Output
+               console.log(a/b);
+               //TASK5 Write A Program To Modulus Two Numbers And Console The Output
+              console.log(a%b);
+
+//ACTIVITY 2 ASSIGNMENT OPERATORS
+
+              //TASK6 Using += Operator To add a number 
+              a+=5;
+              b+=5;
+              console.log(a,b);
+              //TASK& using -= operator
+              a-=5;
+              b-=5;
+              console.log(a,b);
+
+//ACTIVITY 3 COMPARISON OPERATOR
+             
+            //TASK8 Compare Two Numbers Using < and >
+            if(a>b){
+                console.log("largest number is ",a);
+            }
+            else {
+                console(b);
+            }
+            //TASK9 Compare TWo Numbers using <= and >=
+            if(a>=b){
+                console.log("largest number is ",a);
+            }
+            else {
+                console(b);
+            }
+            //TASK10 Compare Two NUmbers Using == ans ===
+            a=20;
+            b=21;
+            if(a==b){
+                console.log("true");
+            }
+            else {
+                console.log(b);
+            }
+            if(a===b){
+                console.log("true");
+            }else{
+                console.log("false");
+            }
+//ACTIVITY 4 LOGICAL OPERATORS
+        
+           //TASK11 Compare the TWo numbers Using && Operator
+           if(a&&b==20){
+            console.log("thats true");
+           }
+           else{
+            console.log("false");
+           }
+           //TASK12 compare the two numbers usimg || Operator
+           if(a||b==20){
+            console.log("thats true");
+           }
+           else{
+            console.log("false");
+           }
+           //TASK13 Compare The Two Numbers Using ! Operator
+           if(a!=20){
+            console.log("thats true");
+           }
+           else{
+            console.log("false");
+           }
+//ACITVITY 5 TERNARY OPERATOR
+           //TASK14 use the tenary Operator 
+           b="course completed"     
+           ans = (b=="course completed" )? "DAY2 COMPLETED":"continue";  
+           console.log(ans);
+           
+
+//DAY2 COMPLETED
+//BRAINBOXCODE
